3784e6ce9f3ca05d3a6a004f7c37eb340eb6ccae
Sankara Subramanian Murthy <56157694+sankar-010897@users.noreply.github.com>
2023-09-10 11:15:27 +0530

    Add files via upload

---
+++ forLoop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (int i = 0; i < 5; i++) {
+	 cout << "I am looping this 5 times" << "\n";
+	}
+	// using for loop to print even numbers
+	for (int i = 0; i <= 10; i = i + 2) {
+		cout << i << "\n";
+	}
+	return 0;
+}
